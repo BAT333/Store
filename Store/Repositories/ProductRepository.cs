3a9f4fa4ae79981a66442f66401cb83a9f86279b
Bat333 <rafaelolivais09@hotmail.com>
2025-11-21 21:02:08 -0300

    PRODUCT CRUD CREATED

--- Store/Repositories/ProductRepository.cs
+++ Store/Repositories/ProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using Store.Models;
+﻿using Store.Infrastructure;
+using Store.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,24 +10,82 @@ namespace Store.Repositories
 {
     internal class ProductRepository : IDao<Product>
     {
+        private readonly SqlConnectionProvider _connectionProvider;
+
+        public ProductRepository(SqlConnectionProvider connectionProvider)
+        {
+            this._connectionProvider = connectionProvider;
+        }
         public Product Add(Product entity)
         {
-            throw new NotImplementedException();
+            string qurey = "INSERT INTO Product(Name,Description,Price) OUTPUT INSERTED.ID " +
+    "VALUES (@Name,@Description,@Price)";
+
+            using var connectionProvider = this._connectionProvider.CreateOpenConnection();
+            using var cmd = connectionProvider.CreateCommand();
+
+            cmd.CommandText = qurey;
+            cmd.AddParam("@Name", entity.Name);
+            cmd.AddParam("@Description", entity.Description);
+            cmd.AddParam("@Price", entity.Price);
+
+            entity.Id = Convert.ToInt32(cmd.ExecuteScalar());
+            return entity;
         }
 
         public bool Delete(int id)
         {
-            throw new NotImplementedException();
+            string query = "DELETE FROM Product WHERE Id = @ID";
+
+            using var connectionProvider = this._connectionProvider.CreateOpenConnection();
+            using var cmd = connectionProvider.CreateCommand();
+
+            cmd.CommandText = query;
+            cmd.AddParam("@ID", id);
+
+            return cmd.ExecuteNonQuery() > 0;
         }
 
         public Product? GetById(int id)
         {
-            throw new NotImplementedException();
+            string query = "SELECT ID, Name, Description ,Price FROM Product WHERE ID = @ID";
+
+            using var connectionProvider = this._connectionProvider.CreateOpenConnection();
+            using var cmd = connectionProvider.CreateCommand();
+
+            cmd.CommandText = query;
+            cmd.AddParam("@ID", id);
+
+            using var reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                int productID = Convert.ToInt32(reader["ID"]);
+                string name = reader["Name"].ToString();
+                string description = reader["Description"].ToString();
+                double price = Convert.ToDouble(reader["Price"]);
+
+                return new Product(productID, name, description, price);
+
+            }
+            return null;
         }
 
-        public Product Update(int id, Product entity)
+        public Product? Update(int id, Product entity)
         {
-            throw new NotImplementedException();
+            string query = "UPDATE Product SET Name = @Name, Description = @Description ,Price = @Price WHERE ID = @ID";
+
+            using var connectionProvider = this._connectionProvider.CreateOpenConnection();
+            using var cmd = connectionProvider.CreateCommand();
+
+            cmd.CommandText = query;
+
+            cmd.AddParam("@Name", entity.Name);
+            cmd.AddParam("@Description", entity.Description);
+            cmd.AddParam("@Price", entity.Price);
+            cmd.AddParam("@ID", id);
+
+            return cmd.ExecuteNonQuery() > 0 ? entity : null;
         }
     }
 }

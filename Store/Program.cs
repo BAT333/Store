69434aaedb07696d8604e33f48d575e1539c49b7
Bat333 <rafaelolivais09@hotmail.com>
2025-11-21 21:50:35 -0300

    GENERAL ADJUSTMENTS

--- Store/Program.cs
+++ Store/Program.cs
@@ -5,6 +5,12 @@ string? value = Environment.GetEnvironmentVariable(
     "DB_CONNECTION",
     EnvironmentVariableTarget.Machine
 );
+
+using Store.Models;
+string? value = Environment.GetEnvironmentVariable(
+    "DB_CONNECTION",
+    EnvironmentVariableTarget.Machine
+);
 SqlConnectionProvider sqlConnection = new SqlConnectionProvider(value);
 
 Console.ReadLine();
